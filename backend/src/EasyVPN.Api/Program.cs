05b0d6df84c73fe5e84653a7314fa6697a5088d4
F1st3K <N1k0s04k@gmail.com>
2024-02-02 17:46:31 +0300

    create global errors handling

--- backend/src/EasyVPN.Api/Program.cs
+++ backend/src/EasyVPN.Api/Program.cs
@@ -1,16 +1,22 @@
+using EasyVPN.Api.Errors;
 using EasyVPN.Application;
 using EasyVPN.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+    
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, ApiProblemsDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
