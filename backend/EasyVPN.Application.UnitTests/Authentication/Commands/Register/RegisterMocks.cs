c2040af29ef9abeb7397079cb612e0db798a2c7e
F1st3K <N1k0s04k@gmail.com>
2024-02-22 00:18:50 +0300

    create register tests and add hash generator

---
+++ backend/EasyVPN.Application.UnitTests/Authentication/Commands/Register/RegisterMocks.cs
@@ -0,0 +1,32 @@
+using EasyVPN.Application.Authentication.Commands.Register;
+using EasyVPN.Application.Common.Interfaces.Authentication;
+using EasyVPN.Application.Common.Interfaces.Persistence;
+using EasyVPN.Application.UnitTests.CommonTestUtils.Constants;
+using EasyVPN.Domain.Common.Enums;
+using EasyVPN.Domain.Entities;
+using Moq;
+
+namespace EasyVPN.Application.UnitTests.Authentication.Commands.Register;
+
+public class RegisterMocks
+{
+    public readonly Mock<IUserRepository> UserRepository = new();
+    public readonly Mock<IUserRoleRepository> UserRoleRepository = new();
+    public readonly Mock<IJwtTokenGenerator> JwtTokenGenerator  = new();
+    public readonly Mock<IHashGenerator> HashGenerator  = new();
+    
+    public RegisterCommandHandler CreateHandler()
+    {
+        JwtTokenGenerator.Setup(x =>
+                x.GenerateToken(It.IsAny<User>(), It.IsAny<IEnumerable<RoleType>>()))
+            .Returns(Constants.User.Token);
+        HashGenerator.Setup(x => 
+                x.Hash(It.IsAny<string>()))
+            .Returns(Constants.User.HashPassword);
+        return new RegisterCommandHandler(
+            UserRepository.Object,
+            UserRoleRepository.Object,
+            JwtTokenGenerator.Object,
+            HashGenerator.Object);
+    }
+}
