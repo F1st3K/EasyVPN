5826a57f65c65144bf8ab9980ff5f9a8ccc27787
F1st3K <N1k0s04k@gmail.com>
2024-03-05 17:02:22 +0300

    add TryRemoveExpire on ExpirationChecker

--- backend/src/EasyVPN.Application/Common/Interfaces/Services/IExpirationChecker.cs
+++ backend/src/EasyVPN.Application/Common/Interfaces/Services/IExpirationChecker.cs
@@ -5,5 +5,6 @@ namespace EasyVPN.Application.Common.Interfaces.Services;
 public interface IExpirationChecker
 {
     public void Run();
-    public void NewExpire(DateTime expireTime, Func<ErrorOr<Success>> onExpire);
+    public Guid NewExpire(DateTime expireTime, Func<ErrorOr<Success>> onExpire);
+    public bool TryRemoveExpire(Guid expireId);
 }
