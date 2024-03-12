9e409554f0fc9714af65cf10140650bae171dc8c
F1st3K <N1k0s04k@gmail.com>
2024-03-12 17:00:15 +0300

    create unit tests ConfirmConnectionTicket

---
+++ backend/tests/UnitTests/EasyVPN.Application.UnitTests/ConnectionTickets/Commands/ConfirmConnectionTicket/ConfirmConnectionTicketTests.cs
@@ -0,0 +1,51 @@
+using EasyVPN.Application.UnitTests.CommonTestUtils.Constants;
+using EasyVPN.Domain.Common.Errors;
+using EasyVPN.Domain.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace EasyVPN.Application.UnitTests.ConnectionTickets.Commands.ConfirmConnectionTicket;
+
+public class CreateConnectionTicketTests
+{
+    private readonly ConfirmConnectionTicketMocks _mocks = new();
+    
+    [Fact]
+    public async Task HandleConfirmConnectionTicketCommand_WhenIsAllValid_Success()
+    {
+        //Arrange
+        var command = CreateConnectionTicketUtils.CreateCommand();
+
+        _mocks.ConnectionTicketRepository.Setup(x
+                => x.Get(Constants.ConnectionTicket.Id))
+            .Returns(new ConnectionTicket() { Id = Constants.ConnectionTicket.Id });
+
+        //Act
+        var handler = _mocks.CreateHandler();
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        //Assert
+        result.IsError.Should().BeFalse(result.FirstError.ToString());
+
+        _mocks.ConnectionTicketRepository.Verify(x =>
+            x.Update(It.Is<ConnectionTicket>(connection => connection.IsValid())));
+    }
+
+    [Fact]
+    public async Task HandleConfirmConnectionTicketCommand_WhenConnectionTicketNotFound_Error()
+    {
+        //Arrange
+        var command = CreateConnectionTicketUtils.CreateCommand();
+
+        _mocks.ConnectionTicketRepository.Setup(x
+                => x.Get(Constants.ConnectionTicket.Id))
+            .Returns(() => null);
+        //Act
+        var handler = _mocks.CreateHandler();
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        //Assert
+        result.FirstError.Should().Be(Errors.ConnectionTicket.NotFound);
+    }
+
+}
