79ac3ba2e3645aa4e3d8a8d095d85a620545196f
F1st3K <N1k0s04k@gmail.com>
2024-03-19 16:02:28 +0300

    add unit tests getConnectionTicket

--- backend/tests/UnitTests/EasyVPN.Application.UnitTests/ConnectionTickets/Commands/CreateConnectionTicket/CreateConnectionTicketUtils.cs
+++ backend/tests/UnitTests/EasyVPN.Application.UnitTests/ConnectionTickets/Commands/CreateConnectionTicket/CreateConnectionTicketUtils.cs
@@ -14,6 +14,7 @@ public static class CreateConnectionTicketUtils
 
     public static bool IsValid(this ConnectionTicket connectionTicket)
         => connectionTicket.ConnectionId == Constants.Connection.Id
+           && connectionTicket.ClientId == Constants.User.Id
            && connectionTicket.Status == ConnectionTicketStatus.Pending
            && connectionTicket.CreationTime == Constants.Time.Now
            && connectionTicket.Days == Constants.ConnectionTicket.Days
