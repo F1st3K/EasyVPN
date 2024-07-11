01c2a2df4c3d4eae1a3b84f658522a524077cc54
F1st3K <N1k0s04k@gmail.com>
2024-07-12 01:34:54 +0300

    icon ConnectionShortItem view

--- frontend/src/modules/ConnectionShortItem/index.tsx
+++ frontend/src/modules/ConnectionShortItem/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Chip, ListItemText } from '@mui/material';
+import { ContentPasteSearch, DeleteForever, PostAdd } from '@mui/icons-material';
+import { Avatar, Box, Chip, IconButton, ListItemAvatar, ListItemText } from '@mui/material';
 import React, { FC } from 'react';
 
 import { Connection } from '../../api';
@@ -8,38 +9,55 @@ const ConnectionShortItem: FC<Connection> = (connection: Connection) => {
     const wart = new Date(new Date(expt).setDate(expt.getDate() - 3));
     return (
         <Box display="flex" flexDirection="row" flexWrap="wrap" alignItems="center" gap={2} width={'100%'}>
-            <ListItemText
-                primary={connection.server.protocol.name}
-                secondary={
-                    <Chip
-                        label={
-                            <>
-                                {new Date() > expt ? <>Expired on</> : <>Expires from</>} {expt.toDateString()} at{' '}
-                                {expt.getHours().toString().padStart(2, '0')}:
-                                {expt.getMinutes().toString().padStart(2, '0')}
-                            </>
-                        }
-                        variant="outlined"
-                        color={new Date() > wart ? (new Date() > expt ? 'error' : 'warning') : 'info'}
-                        size="small"
-                    />
-                }
-            />
+            <Box display="flex" flexDirection="row" alignItems="center">
+                <ListItemAvatar>
+                    <Avatar src={connection.server.protocol.icon} />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={connection.server.protocol.name}
+                    secondary={
+                        <Chip
+                            label={
+                                <>
+                                    {new Date() > expt ? <>Expired on</> : <>Expires from</>} {expt.toDateString()} at{' '}
+                                    {expt.getHours().toString().padStart(2, '0')}:
+                                    {expt.getMinutes().toString().padStart(2, '0')}
+                                </>
+                            }
+                            variant="outlined"
+                            color={new Date() > wart ? (new Date() > expt ? 'error' : 'warning') : 'info'}
+                            size="small"
+                        />
+                    }
+                />
+            </Box>
             <Box display="flex" justifyContent="space-between" alignItems="center" flex="1">
                 <Box display="flex" gap={1}>
-                    <Chip label="Get config" color="info" onClick={() => console.log('Get config clicked')} />
-                    <Chip label="Extend" color="success" onClick={() => console.log('Extend clicked')} />
+                    <Chip
+                        label="Config"
+                        icon={<ContentPasteSearch />}
+                        color="info"
+                        onClick={() => console.log('Get config clicked')}
+                    />
+                    <Chip
+                        variant="filled"
+                        label="Extend"
+                        icon={<PostAdd />}
+                        color="success"
+                        onClick={() => console.log('Extend clicked')}
+                    />
                 </Box>
-                <Chip
+                <IconButton
+                    aria-label="delete"
                     sx={{
-                        visibility: expt.getMinutes() != 27 ? 'visible' : 'hidden',
-                        marginLeft: 4,
-                        marginRight: 2,
+                        visibility: new Date() > expt ? 'visible' : 'hidden',
+                        marginX: 1,
                     }}
-                    label="Delete"
                     color="error"
                     onClick={() => console.log('Delete clicked')}
-                />
+                >
+                    <DeleteForever />
+                </IconButton>
             </Box>
         </Box>
     );
