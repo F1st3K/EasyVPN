ed3cc0ab80492b3ac45b96fadb98269381f3a03b
F1st3K <N1k0s04k@gmail.com>
2024-08-02 14:19:05 +0300

    --amend

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-RELEASE_VERSION= cat VERSION
+RELEASE_VERSION=$(head -n 1 VERSION)
 
 # Finish release by git flow
-git tag -a $RELEASE_VERSION -m "Release version $RELEASE_VERSION"
+git tag -a "$RELEASE_VERSION" -m "Release version $RELEASE_VERSION"
 git push origin "release/$RELEASE_VERSION"
 
 # Check status
