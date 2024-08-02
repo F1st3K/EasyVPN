3d2ea68ad4cb4f5344af614c6dbb1730649ec06d
F1st3K <N1k0s04k@gmail.com>
2024-08-02 13:45:49 +0300

    bugfix(release): fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,8 @@
 RELEASE_VERSION= cat VERSION
 
 # Finish release by git flow
-git flow release finish -m "Release version $RELEASE_VERSION" "$RELEASE_VERSION"
+git tag -a $RELEASE_VERSION -m "Release version $RELEASE_VERSION"
+git push origin "release/$RELEASE_VERSION"
 
 # Check status
 if [ $? -ne 0 ]; then
