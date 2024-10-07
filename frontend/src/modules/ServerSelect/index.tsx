f566e7b15edf83fd5c7673d3c500cef813fd5eb7
F1st3K <N1k0s04k@gmail.com>
2024-10-07 13:39:45 +0300

    fix server select component

--- frontend/src/modules/ServerSelect/index.tsx
+++ frontend/src/modules/ServerSelect/index.tsx
@@ -1,7 +1,9 @@
 import { Autocomplete, Box, TextField } from '@mui/material';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 
+import { Context } from '../..';
 import EasyVpn, { ApiError, Server, VpnVersion } from '../../api';
+import { useRequest } from '../../hooks';
 
 interface ServerSelectProps {
     serverId?: string;
@@ -9,40 +11,24 @@ interface ServerSelectProps {
 }
 
 const ServerSelect: FC<ServerSelectProps> = (props) => {
-    const [servers, setServers] = useState<Server[]>([
-        {
-            id: '00000000-0000-0000-0000-000000000000',
-            protocol: {
-                id: '00000000-0000-0000-0000-000000000000',
-                name: 'WireGuard',
-                icon: 'https://static-00.iconduck.com/assets.00/wireguard-icon-256x256-bdlmygje.png',
-            },
-            version: VpnVersion.V1,
-        },
-        {
-            id: '10000000-0000-0000-0000-000000000000',
-            protocol: {
-                id: '00000000-0000-0000-0000-000000000000',
-                name: 'vireGuard',
-                icon: 'https://static-00.iconduck.com/assets.00/wireguard-icon-256x256-bdlmygje.png',
-            },
-            version: VpnVersion.V1,
-        },
-    ]);
-    const [server, setServer] = useState<Server | null>(
-        servers.find((s) => s.id == props.serverId) || null,
+    const { Auth } = useContext(Context);
+
+    const [servers, loading] = useRequest<Server[], ApiError>(() =>
+        EasyVpn.servers.getAll(Auth.getToken()).then((v) => v.data),
     );
 
+    const server = servers?.find((s) => s.id == props.serverId) || null;
+
     useEffect(() => {
-        props.onChange && props.onChange(server);
+        loading == false && props.onChange && props.onChange(server);
     }, [server]);
 
     return (
         <Autocomplete
             sx={{ width: '23ch' }}
             autoHighlight
-            options={servers}
-            onChange={(_, s) => setServer(s)}
+            options={servers ?? []}
+            onChange={(_, s) => props.onChange && props.onChange(s)}
             value={server}
             getOptionLabel={(o) => o.protocol.name}
             renderOption={(p, o) => {
