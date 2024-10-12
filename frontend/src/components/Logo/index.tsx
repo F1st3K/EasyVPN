3a9e469712ebb19eea04561d27f39041851249c0
F1st3K <N1k0s04k@gmail.com>
2024-10-12 15:25:54 +0300

    create dynamic Logo component

---
+++ frontend/src/components/Logo/index.tsx
@@ -0,0 +1,33 @@
+import { Box, BoxProps, useTheme } from '@mui/material';
+import React, { FC } from 'react';
+
+interface LogoProps extends BoxProps {
+    size?: number;
+    theme?: 'dark' | 'light';
+}
+
+const Logo: FC<LogoProps> = (props) => {
+    const theme = useTheme();
+
+    return (
+        <Box
+            sx={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+            }}
+            {...props}
+            width={props.size || 25}
+        >
+            <img
+                loading="eager"
+                width="100%"
+                height="100%"
+                src={`EasyVPN-logo.${props.theme || theme.palette.mode}.png`}
+                alt=""
+            />
+        </Box>
+    );
+};
+
+export default Logo;
