bea71777fbc69a25844398b3a12b833984f6cbc7
Alex Poryadin <alex.poryadih@gmail.com>
2025-04-05 23:35:58 +0300

    add start route to bot

--- services/TelegramBot/bot/core/config.py
+++ services/TelegramBot/bot/core/config.py
@@ -1,5 +1,13 @@
+from __future__ import annotations
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+DIR = Path(__file__).absolute().parent.parent.parent
+BOT_DIR = Path(__file__).absolute().parent.parent
+LOCALES_DIR = f"{BOT_DIR}/locales"
+I18N_DOMAIN = "messages"
+DEFAULT_LOCALE = "en"
 
 class EnvBaseSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
