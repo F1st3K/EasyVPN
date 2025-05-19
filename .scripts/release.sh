34b4a84e37e4042b9f45813e136291847d15f97d
Nikita Kostin <n1k0s04k@gmail.com>
2025-05-20 01:10:42 +0300

    chore(release): increment global version to 1.0.0

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -8,7 +8,7 @@ fi
 NEW_VERSION="$1"
 
 git checkout -b release/$NEW_VERSION && \
-    docker build -t dev-env ./.scripts/ && \
+#    docker build -t dev-env ./.scripts/ && \
     docker run --rm -v "$PWD":/app dev-env ./.scripts/set-global-version.sh $NEW_VERSION && \
     git add . && \
     git commit -am "chore(release): increment global version to $NEW_VERSION"
