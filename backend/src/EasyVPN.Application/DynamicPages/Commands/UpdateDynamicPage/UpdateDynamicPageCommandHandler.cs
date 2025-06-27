b407cc094485b53d24455d5dda7a67905ae045a2
Nikita Kostin <n1k0s04k@gmail.com>
2025-06-27 17:51:05 +0300

    refactor logic for update dp to correctly EF

--- backend/src/EasyVPN.Application/DynamicPages/Commands/UpdateDynamicPage/UpdateDynamicPageCommandHandler.cs
+++ backend/src/EasyVPN.Application/DynamicPages/Commands/UpdateDynamicPage/UpdateDynamicPageCommandHandler.cs
@@ -1,7 +1,6 @@
 using EasyVPN.Application.Common.Interfaces.Persistence;
 using EasyVPN.Application.Common.Interfaces.Services;
 using EasyVPN.Domain.Common.Errors;
-using EasyVPN.Domain.Entities;
 using ErrorOr;
 using MediatR;
 
@@ -25,23 +24,20 @@ public class UpdateDynamicPageCommandHandler : IRequestHandler<UpdateDynamicPage
         if (_dynamicPageRepository.Get(command.Route) is not { } page)
             return Errors.DynamicPage.NotFound;
 
-        var newPage = new DynamicPage
-        {
-            Route = command.NewRoute,
-            Title = command.Title,
-            Content = command.Content,
-            Created = page.Created,
-            LastModified = _dateTimeProvider.UtcNow,
-        };
+        page.Title = command.Title;
+        page.Content = command.Content;
+        page.LastModified = _dateTimeProvider.UtcNow;
 
-        if (page.Route == newPage.Route)
+        if (page.Route == command.NewRoute)
         {
-            _dynamicPageRepository.Update(newPage);
+            _dynamicPageRepository.Update(page);
         }
         else
         {
             _dynamicPageRepository.Remove(page.Route);
-            _dynamicPageRepository.Add(newPage);
+            
+            page.Route = command.NewRoute;
+            _dynamicPageRepository.Add(page);
         }
 
         return Result.Updated;
